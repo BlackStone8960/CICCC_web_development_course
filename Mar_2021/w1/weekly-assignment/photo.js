964d8536df772eb8e83595d429bbc5d1a9d498a6
Taichi I <fng.trf.kk1020@gmail.com>
2021-03-07 18:57:12 -0800

    fix orders of asynchronous processes

--- Mar_2021/w1/weekly-assignment/photo.js
+++ Mar_2021/w1/weekly-assignment/photo.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const photoList = document.getElementById('photolist');
-const seeMoreBtn = document.getElementById('btnMore');
 const loadingImg = document.getElementById('loadingImg');
 const selectElement = document.getElementById('loadNum');
 let loadNum = selectElement.value; // The number of elements which will be loaded when "See More" button clicked
-let dataLength;
+let dataLength, loadedItemLength;
 
 // Function to output json formatted information to HTML
 const renderJSON = (json, times, currentPosition) => {
-  for(let i = 0; i < times; i++) {
+  for (let i = 0; i < times; i++) {
     const j = i + currentPosition;
     const thumbnail = json[j].thumbnailUrl;
     const title = json[j].title;
@@ -29,12 +28,13 @@ const fetchAPI = () => {
   .then((res) => {
     if(res.status !== 200) {
       console.log(`Something went wrong. Status code: ${res.status}`);
+      return;
     }
     return res.json();
   })
   .then((data) => { // success
     dataLength = data.length;
-    const loadedItemLength = photoList.getElementsByClassName('loaded').length;
+    loadedItemLength = photoList.getElementsByClassName('loaded').length;
     const restLength = dataLength - loadedItemLength;
 
     if(dataLength !== loadedItemLength) { // If datum to load remain
@@ -42,7 +42,7 @@ const fetchAPI = () => {
         renderJSON(data, loadNum, loadedItemLength);
       } else {
         renderJSON(data, restLength, loadedItemLength);
-        seeMoreBtn.remove(); // process ends
+        document.getElementById('btnMore').remove(); // process ends
       } 
     } else { // If there are no datum to load
       return false; // process ends
@@ -51,17 +51,19 @@ const fetchAPI = () => {
   .catch((err) => {
     console.log(`Error detected. ${err}`);
   })
-  .finally(() => { // This method is executed regardless of whether the process successes or fails.
+  .finally(() => {
     loadingImg.classList.add('hide'); // Hide loading animation
-  });
+    if (!document.getElementById('btnMore') && dataLength !== photoList.getElementsByClassName('loaded').length) {
+      photoList.insertAdjacentHTML('afterend', `<div id="btnMore">See More</div>`);
+      document.getElementById('btnMore').addEventListener('click', () => {
+        loadingImg.classList.remove("hide"); // Make loading animation appears
+        fetchAPI();        
+      });
+    }
+  })
 };
 
 fetchAPI();
-
-seeMoreBtn.addEventListener('click', () => {
-  loadingImg.classList.remove("hide"); // Make loading animation appears
-  fetchAPI();
-});
 
 selectElement.addEventListener('change', () => {
   loadNum = selectElement.value !== "all" ? selectElement.value : dataLength;
