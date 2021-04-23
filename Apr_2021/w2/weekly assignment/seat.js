3ff6b9b9c12f910086e0727b4d51e0b41d5b0be1
Taichi I <fng.trf.kk1020@gmail.com>
2021-04-23 02:26:17 -0700

    added some assignment

---
+++ Apr_2021/w2/weekly assignment/seat.js
@@ -0,0 +1,60 @@
+let activeSeats = 0;
+let admissionFee = 15;
+const seatEl = document.getElementsByClassName('seat');
+
+const renderTotal = (userSelection) => {
+  document.getElementById('seats-num').innerText = userSelection ? userSelection.activeSeats : 0;
+  document.getElementById('total').innerText = userSelection ? userSelection.activeSeats * userSelection.admissionFee : 0;
+};
+
+const recordSeatsStatus = () => {
+  let seatStatus = [];
+  for(let i = 0; i < seatEl.length; i++) {
+    if (seatEl[i].classList.contains('active')) {
+      seatStatus.push(1);
+    } else if (seatEl[i].classList.contains('reserved')) {
+      seatStatus.push(2);
+    } else {
+      seatStatus.push(0);
+    }
+  }
+  return seatStatus;
+};
+
+const seatActive = (element) => {
+  if (element.classList.contains('reserved')) return;
+  element.classList.toggle('active');
+  activeSeats = document.getElementsByClassName('active').length;
+  const userSelection = { activeSeats, admissionFee };
+  renderTotal(userSelection);
+  userSelection.seatsStatus = recordSeatsStatus();
+  localStorage.setItem('userSelection', JSON.stringify(userSelection));
+};
+
+const renderSeatsStatus = (seatsStatus) => {
+  for(let i = 0; i < seatsStatus.length; i++) {
+    switch(seatsStatus[i]) {
+      case 1 :
+        seatEl[i].classList.add('active');
+        break;
+      case 2 :
+        seatEl[i].classList.add('reserved');
+        break;
+      default :
+        continue;
+    }
+  }
+};
+
+window.addEventListener('load', () => {
+  const userSelection = JSON.parse(localStorage.getItem('userSelection'));
+  // take datum from localstorage
+  renderTotal(userSelection);
+  let seatsStatus;
+  if (userSelection.seatsStatus) {
+    seatsStatus = userSelection.seatsStatus;
+  } else {
+    seatsStatus = recordSeatsStatus();
+  }
+  renderSeatsStatus(seatsStatus);
+});
