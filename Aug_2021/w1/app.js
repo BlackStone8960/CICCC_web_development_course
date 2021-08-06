9227a779d171e41c0b9e48e186a7bd381eaaaaf8
Taichi I <fng.trf.kk1020@gmail.com>
2021-08-05 22:12:53 -0700

    added fortune telling app

--- Aug_2021/w1/app.js
+++ Aug_2021/w1/app.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const resultPath = path.join(__dirname, 'JSON', 'fortuneTellingResult.json');
+const arg = process.argv[2];
+const dataJSON = fs.readFileSync(resultPath).toString();
+const data = JSON.parse(dataJSON);
 
-switch (process.argv[2]) {
-  case 'fortune':
-    const dataJSON = fs.readFileSync(resultPath).toString();
-    const data = JSON.parse(dataJSON);
-    const dataLength = Object.keys(data).length;
-    const randomNum = Math.floor(Math.random() * (dataLength));
-    console.log(data[Object.keys(data)[randomNum]]);
-    break;
-  default:
-    break;
+if (arg === "love" || arg === "work" || arg === "money") {
+  const resultsArr = data[arg];
+  const randomNum = Math.floor(Math.random() * (resultsArr.length));
+  console.log(resultsArr[randomNum]);
+} else {
+  console.log("To have fortune told, type love or work or money as an argument")
 };
