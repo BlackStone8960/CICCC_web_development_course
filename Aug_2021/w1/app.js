c5bbeb336a9b1276caf3f82c75bff67fba7b720d
Taichi I <fng.trf.kk1020@gmail.com>
2021-08-05 22:17:30 -0700

    edited fortune telling app a little

--- Aug_2021/w1/app.js
+++ Aug_2021/w1/app.js
@@ -1,3 +1,6 @@
+// This is a fortune telling app.
+// To run this program, type "work" or "love" or "money" as an argument.
+
 const fs = require('fs');
 const path = require('path');
 const resultPath = path.join(__dirname, 'JSON', 'fortuneTellingResult.json');
@@ -5,7 +8,7 @@ const arg = process.argv[2];
 const dataJSON = fs.readFileSync(resultPath).toString();
 const data = JSON.parse(dataJSON);
 
-if (arg === "love" || arg === "work" || arg === "money") {
+if (arg === "work" || arg === "love" || arg === "money") {
   const resultsArr = data[arg];
   const randomNum = Math.floor(Math.random() * (resultsArr.length));
   console.log(resultsArr[randomNum]);
